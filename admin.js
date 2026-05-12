c2e44849d63c054606368c7bb4479ff48b8538cc
parkss9799 <parkss9799@gmail.com>
2026-05-12 11:39:25 +0900

    update latest api

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
 const API_URL =
-"https://script.google.com/macros/s/AKfycbyuvviYB_g2Py-G4wT-X0Ei329OGN1m2mKK2MZugQHSTXlQONbE6ykev-Kah303vAK-qQ/exec";
+"https://script.google.com/macros/s/AKfycbwnbDQyPvvFFVsadq4PP7aPjOZZbPA--cXsI2_AqlfM2O52KsrLc5O22FHm1nBSrtTgVw/exec";
 
 window.onload = function () {
   const login = localStorage.getItem("tk_admin_login");

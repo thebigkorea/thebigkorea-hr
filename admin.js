9a0543d3104cc982bab5af3835ddfdd6fedf2e04
parkss9799 <parkss9799@gmail.com>
2026-05-12 13:40:39 +0900

    fix spreadsheet id

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
 const API_URL =
-"https://script.google.com/macros/s/AKfycbyWzaqWVY4hNKWXK5cHz6Ipcat5WlFpVaTAYHefwqevLvMZrm41Oebsiv75-ACBP9BYzA/exec";
+"https://script.google.com/macros/s/AKfycbwnR6KiBXhmhLY4n-KURUl0So8LXzecUfglFHYUx-RCG9MnwV41zu93x9fl6EkRYeb3JQ/exec";
 
 window.onload = function () {
 
